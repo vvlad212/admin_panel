f591ef60054d51d2b053d63858c0bf1a94f2b8d9
Vladislav Zhujkov <velocrosss@yandex.ru>
2022-04-27 23:12:20 +0300

    подключился к elastick, написал тестовый bulk запрос на внесение запрос отработал
    была проблема с подключением к elastick, для версии elastick 7.7.0 можно использовать библиотеку не выше 7.17

--- 01_etl/elastick_request.py
+++ 01_etl/elastick_request.py
@@ -1,12 +1,46 @@
+from elasticsearch import Elasticsearch
+
+# Create the client instance
+client = Elasticsearch("http://127.0.0.1:9200")
+
+# Successful response!
+a = client.info()
+
 resp = client.bulk(
     body=[
-        {"index": {"_index": "test", "_id": "1"}},
-        {"field1": "value1"},
-        {"delete": {"_index": "test", "_id": "2"}},
-        {"create": {"_index": "test", "_id": "3"}},
-        {"field1": "value3"},
-        {"update": {"_id": "1", "_index": "test"}},
-        {"doc": {"field2": "value2"}},
+        {"index": {"_index": "movies", "_id": "1"}},
+        {
+            "imdb_rating": "5",
+            "genre": 3,
+            "title": 'test_title',
+            "description": 'test_description',
+            "director": 'director1 director2',
+            "actors_names": 'actors_name1 actors_name2',
+            "writers_names": 'test_description',
+            "actors": [
+                {
+                    'id': 'test_actors_id1',
+                    'name': 'test_actors_name1'
+                },
+                {
+                    'id': 'test_actors_id2',
+                    'name': 'test_actors_name2'
+                }
+            ],
+            "writers": [
+                {
+                    'id': 'test_writers_id1',
+                    'name': 'test_writers_name1'
+                },
+                {
+                    'id': 'test_writers_id2',
+                    'name': 'test_writers_name2'
+                }
+            ],
+        }
     ],
 )
-print(resp)
+
+
+
+pass
---
+++ 01_etl/state.py
@@ -0,0 +1,67 @@
+import abc
+from typing import Any, Optional
+import json
+
+
+class BaseStorage:
+    @abc.abstractmethod
+    def save_state(self, state: dict) -> None:
+        """Сохранить состояние в постоянное хранилище"""
+        pass
+
+    @abc.abstractmethod
+    def retrieve_state(self) -> dict:
+        """Загрузить состояние локально из постоянного хранилища."""
+        pass
+
+
+class JsonFileStorage(BaseStorage):
+    def __init__(self, file_path: Optional[str] = None):
+        self.file_path = file_path
+
+    def retrieve_state(self) -> dict:
+        """Загрузить состояние локально из постоянного хранилища."""
+        with open(self.file_path, 'r') as file:
+            state_file = file.read()
+            if state_file != '':
+                return json.loads(state_file)
+            else:
+                return {}
+
+    def save_state(self, state: dict) -> None:
+        """Сохранить состояние в постоянное хранилище"""
+        with open(self.file_path, 'w') as file:
+            file.write(json.dumps(state))
+
+
+class State:
+    """
+    Класс для хранения состояния при работе с данными, чтобы постоянно
+    не перечитывать данные с начала.
+    Здесь представлена реализация с сохранением состояния в файл.
+    В целом ничего не мешает поменять это поведение на работу с
+    БД или распределённым хранилищем.
+    """
+
+    def __init__(self, storage: BaseStorage):
+        self.storage = storage
+
+    def set_state(self, key: str, value: Any) -> None:
+        """Установить состояние для определённого ключа"""
+        self.storage.save_state({key: value})
+
+    def get_state(self, key: str) -> Any:
+        """Получить состояние по определённому ключу"""
+        state_dict = self.storage.retrieve_state()
+        if state_dict.keys().__contains__(key):
+            return state_dict[key]
+        else:
+            return None
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    storage = JsonFileStorage()
+    storage.file_path = '/Users/vladislavzujkov/PycharmProjects/Yandex_learn/test.json'
+    state = State(storage).get_state('modifie')
+    State(storage).set_state('1', 'sstate')
+    pass
