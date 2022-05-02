a2cb1628d6fe9c04a1c18600935fa5a79b038c80
vladislavzujkov <velocrosss@gmail.com>
2022-05-02 21:18:39 +0300

    Поменял запрос, теперь даныне об обновлениях получаются со всех таблиц (добавил  WHERE fw.modified > '{time_select}' or g.modified > '{time_select}' or p.modified > '{time_select}')

--- 01_etl/queries.py
+++ 01_etl/queries.py
@@ -4,24 +4,23 @@ def get_all(time_select):
     fw.modified,
     fw.id,
     fw.rating as imdb_rating,
-    array_agg(DISTINCT g.name) filter (WHERE g.name is not null) as genre,
+    array_agg(DISTINCT g.name) filter (WHERE g.name is not null)                                as genre,
     fw.title,
-    fw.description as description,
-    coalesce(array_agg(DISTINCT p.full_name) filter (WHERE pfw.id is not null and pfw.role = 'director'), '{{}}'
-           )     as director, 
-    array_agg( DISTINCT p.full_name) filter (WHERE pfw.id is not null and pfw.role = 'actor') as actors_names,
-    array_agg( DISTINCT p.full_name) filter (WHERE pfw.id is not null and pfw.role = 'writer') as writers_names,
+    fw.description                                                                              as description,
+    coalesce(array_agg(DISTINCT p.full_name) 
+        FILTER (WHERE pfw.id is not null and pfw.role = 'director'), '{{}}')                    as director, 
+    array_agg( DISTINCT p.full_name) filter (WHERE pfw.id is not null and pfw.role = 'actor')   as actors_names,
+    array_agg( DISTINCT p.full_name) filter (WHERE pfw.id is not null and pfw.role = 'writer')  as writers_names,
     json_agg( DISTINCT jsonb_build_object('id', p.id, 'name', p.full_name) ) 
-    FILTER (WHERE p.id is not null and pfw.role = 'actor')as actors,
+        FILTER (WHERE p.id is not null and pfw.role = 'actor')                                  as actors,
     json_agg( DISTINCT jsonb_build_object('id', p.id,'name', p.full_name)) 
-    FILTER (WHERE p.id is not null and pfw.role = 'writer') as writers
+        FILTER (WHERE p.id is not null and pfw.role = 'writer')                                 as writers
     FROM content.film_work fw
-             LEFT JOIN content.person_film_work pfw
-                       ON pfw.film_work_id = fw.id
-             LEFT JOIN content.person p ON p.id = pfw.person_id
-             LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
-             LEFT JOIN content.genre g ON g.id = gfw.genre_id
-    WHERE fw.modified > '{time_select}'
+             LEFT JOIN content.person_film_work pfw                             ON pfw.film_work_id = fw.id
+             LEFT JOIN content.person p                                         ON p.id = pfw.person_id
+             LEFT JOIN content.genre_film_work gfw                              ON gfw.film_work_id = fw.id
+             LEFT JOIN content.genre g                                          ON g.id = gfw.genre_id
+    WHERE fw.modified > '{time_select}' or g.modified > '{time_select}' or p.modified > '{time_select}'
     GROUP BY fw.id
     ORDER BY fw.modified;
 """
